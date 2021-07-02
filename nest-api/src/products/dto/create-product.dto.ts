e4ae68ec2591d3ae88488392e78a92ddd18a507a
Antonio Sergio G Ribeiro <asergio68@gmail.com>
2021-07-02 20:47:11 -0300

    first commit

---
+++ nest-api/src/products/dto/create-product.dto.ts
@@ -0,0 +1,19 @@
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsPositive()
+  @IsNumber()
+  @IsNotEmpty()
+  price: number;
+}
